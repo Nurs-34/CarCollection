dfcb68c821904cb0c40097d87db62d5974ea77ff
Nurs-34 <nurs.ryo@gmail.com>
2023-05-11 00:28:32 +0600

    feat commit: fragment item sketch and icons add

--- app/src/main/java/kg/surfit/carcollection/MainActivity.kt
+++ app/src/main/java/kg/surfit/carcollection/MainActivity.kt
@@ -1,7 +1,7 @@
 package kg.surfit.carcollection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
